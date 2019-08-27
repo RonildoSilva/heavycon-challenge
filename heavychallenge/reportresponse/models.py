4a258f294026d6306d384c92f6c704b62eb9ff65
Ronildo Silva <ronildo.comp@gmail.com>
2019-08-27 20:19:42 -0300

    Corrections about migrations

--- heavychallenge/reportresponse/models.py
+++ heavychallenge/reportresponse/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from report.models import Report
 
 class ReportResponse(models.Model):
-    message=models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
+    report  = models.ForeignKey(Report, on_delete=models.CASCADE, default='')
 
     def __str__(self):
         return self.message
