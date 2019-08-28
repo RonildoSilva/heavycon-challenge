46dbc20437bd7fd3794067e9a75c4949a850aa90
Ronildo Silva <ronildo.comp@gmail.com>
2019-08-28 04:51:19 -0300

    Added relations between users and supervisors

--- heavychallenge/report/models.py
+++ heavychallenge/report/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Report(models.Model):
     message=models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    supervisors = models.ManyToManyField(User, related_name='supervisors')
 
     def __str__(self):
         return self.message
