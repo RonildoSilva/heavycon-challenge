6571a6ed5902fc7dc1ce470087e67da037601ba2
Ronildo Silva <ronildo.comp@gmail.com>
2019-08-30 18:46:34 -0300

    Created a nest serialization for Supervisors

--- heavychallenge/report/models.py
+++ heavychallenge/report/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Report(models.Model):
     message=models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default='', related_name='author')
     supervisors = models.ManyToManyField(User, related_name='supervisors')
 
     def __str__(self):
@@ -18,3 +18,10 @@ class ReportResponse(models.Model):
 
     def __str__(self):
         return self.message
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username        
--- heavychallenge/report/serializers.py
+++ heavychallenge/report/serializers.py
@@ -3,21 +3,31 @@ from . models import Report
 from . models import ReportResponse
 from django.contrib.auth.models import User
 
+class userSerializer(serializers.ModelSerializer):    
+
+    class Meta:
+        model = User
+        fields = ('id','username')
+        #fields = '__all__'
+
 class reportResponseSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = ReportResponse
         fields = '__all__'
 
 class reportSerializer(serializers.ModelSerializer):
-    
+
+    supervisors = userSerializer(read_only=True, many=True)
+
     class Meta:
         model = Report
-        fields = '__all__'
-
-
-class userSerializer(serializers.ModelSerializer):
+        fields = ('message', 'author', 'supervisors')
+        #fields = '__all__'
     
-    class Meta:
-        model = User
-        fields = '__all__'
+    def create(self, validated_data):
+        supervisors_data = validated_data.pop('supervisors')
+        report = Report.objects.create(**validated_data)
+        for supervisor_data in supervisors_data:
+            User.objects.create(report=report, **supervisor_data)
+        
+        return report
